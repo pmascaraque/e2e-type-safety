f15cfebedd540c61b76c3ab8e0a421f5434b1cba
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-27 16:34:22 +0200

    finished front with branches

--- client/src/components/MessageDisplay.tsx
+++ client/src/components/MessageDisplay.tsx
@@ -1,4 +1,5 @@
 import type { Message } from "../types";
+import Branch from "./branch";
 
 type Props = {
   index: Number;
@@ -8,6 +9,7 @@ type Props = {
 function MessageDisplay({ message, index }: Props) {
   return (
     <div className='group mb-2 shrink-0 rounded-lg flex items-center drop-shadow-md bg-zinc-700 w-48 h-20 relative'>
+      <Branch trunk={index === 0} />
       <p className='text-sm text-gray-200 font-bold px-4'>{message.body}</p>
     </div>
   );
