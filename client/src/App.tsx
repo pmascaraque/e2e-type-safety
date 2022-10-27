bd129f9424146ad512cd74d1be72dd6149819e2c
Pablo Moreno <ssm.pablo@gmail.com>
2022-10-27 16:16:31 +0200

    User Display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,28 @@
+import type { User } from "./types";
+import UserDisplay from "./components/UserDisplay";
 function App() {
-  return <h2 className="text-4xl text-red-500">Hi</h2>;
+  const users: User[] = [
+    {
+      //mock data
+      name: "Sabin Adams",
+      messages: [
+        {
+          body: "Hey There!",
+        },
+        {
+          body: "Whats up!",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className='bg-zinc-800 flex-col h-screen w-full flex items-center justify-center'>
+      {users.map((user, index) => (
+        <UserDisplay user={user} key={index} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
